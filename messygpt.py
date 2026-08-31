867b0e057ac07d9f85e131f272e86e84e8386986
ctling <251240041@smail.nju.edu.cn>
2026-08-31 15:30:15 +0800

    feat: part tokenizer complete.

--- messygpt.py
+++ messygpt.py
@@ -18,7 +18,7 @@ dropout = 0.2
 
 torch.manual_seed(1337)
 
-with open('input.txt', 'r', encoding='utf-8') as f:
+with open('src/input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 chars      = sorted(list(set(text)))
