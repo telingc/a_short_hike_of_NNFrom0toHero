88d2283d70ca6d4dfd42296776742d75308ab1ae
ctling <251240041@smail.nju.edu.cn>
2026-09-08 21:58:13 +0800

    feat: 1hr to go. this version is NOT validated and multimember training hasnt been implemented.

--- train_gpt2.py
+++ train_gpt2.py
@@ -32,11 +32,13 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
 
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
-        att = F.softmax(att, dim = -1)
+        # att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        # att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        # att = F.softmax(att, dim = -1)
+        # y = att @ v
 
-        y = att @ v
+        y = F.scaled_dot_product_attention(q, k, v, is_causal = True)
+
         y = y.transpose(1, 2).contiguous().view(B, T, C)
         y = self.c_proj(y)
 
@@ -180,6 +182,31 @@ class GPT(nn.Module):
 
         return model
 
+    def configure_optimizers(self, weight_decay, learning_rate, device_type):
+        # start with all of the candidate parameters (that require grad)
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        # if master_process:
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == "cuda"
+        # if master_process:
+        print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
+
 # ---
 
 class DataLoaderLite:
@@ -214,20 +241,16 @@ if torch.cuda.is_available():
 elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
-# device = 'cpu'
 
-# enc = tiktoken.get_encoding('gpt2')
-# with open('src/input.txt', 'r') as f:
-#     text = f.read()
-# text = text[:1000]
-# tokens = enc.encode(text)
-# B, T = 4, 32
-# buf = torch.tensor(tokens[:B * T + 1])
-# buf = buf.to(device)
-# x = buf[:-1].view(B, T)
-# y = buf[1: ].view(B, T)
+total_batch_size = 2 ** 19
+B = 16
+T = 1024
+assert total_batch_size % (B * T) == 0, "make sure total_batch_size is divisible by B * T."
+grad_accum_steps = total_batch_size // (B * T)
+print(f"total desired batch size: {total_batch_size}")
+print(f"=> calculated gradient accumulation steps: {grad_accum_steps}")
 
-train_loader = DataLoaderLite(B = 16, T = 1024)
+train_loader = DataLoaderLite(B = B, T = T)
 
 torch.set_float32_matmul_precision('high')
 
@@ -236,21 +259,46 @@ model.to(device)
 model = torch.compile(model)
 # logits, loss = model(x, y)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-4)
-for i in range(50):
+max_lr       = 6e-4
+min_lr       = max_lr * 0.1
+warmup_steps = 10
+max_steps    = 50
+
+def get_lr(it):
+    if it < warmup_steps:
+        return max_lr * (it + 1) / warmup_steps
+    if it > max_steps:
+        return min_lr
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (max_lr - min_lr)
+
+# optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-4, betas = (0.9, 0.95))
+optimizer = model.configure_optimizers(weight_decay = 0.1, learning_rate = 6e-4, device = device)
+
+for step in range(50):
     t0 = time.time()
-    x, y = train_loader.next_batch()
-    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    with torch.autocast(device_type = device, dtype = torch.bfloat16):
-        logits, loss = model(x, y)
-        code.interact(local = locals())
-    loss.backward()
+    loss_accum = 0.0
+    for micro_steps in range(grad_accum_steps):
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        with torch.autocast(device_type = device, dtype = torch.bfloat16):
+            logits, loss = model(x, y)
+            code.interact(local = locals())
+        loss /= grad_accum_steps
+        loss_accum += loss.detach()
+        loss.backward()
+    norm = nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
     optimizer.step()
     torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1 - t0) * 1000
-    print(f"step {i}, loss: {loss.item()}, dt: {dt: .2f}ms")
+    print(f"step {step: 4d}, loss: {loss.item()}, lr: {lr: .4e}, norm: {norm: .4}, dt: {dt: .2f}ms")
 
 print(loss)
 sys.exit(0)
